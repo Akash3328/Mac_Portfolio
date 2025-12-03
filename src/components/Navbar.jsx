aad36ce996b3b9535d69922b2e945c74d6d6a7b0
Akash Odedara <akashodedara7@gmail.com>
2025-12-04 01:36:47 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import dayjs from "dayjs";
+import { navLinks ,navIcons } from "#constants";
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src='/images/logo.svg' className='icon' />
+        <p className='font-bold'>Akash's Portfolio</p>
+         <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+            {navIcons.map(({id, img}) => (
+                <li key={id}>
+                    <img src={img} className="icon-hover" alt={`icon-${id}`} />
+
+                </li>
+            ))}
+        </ul>
+        <time>{dayjs().format("ddd , d MMM  hh:mm A")}</time>
+      </div>
+     
+    </nav>
+  );
+};
+
+export default Navbar;
